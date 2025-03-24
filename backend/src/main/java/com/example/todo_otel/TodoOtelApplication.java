44771d05fed3c24410b7c0e22fcbacff50f4abcd
tamago0224 <bigtreesasuke200733@gmail.com>
2025-03-25 00:00:25 +0900

    init

---
+++ backend/src/main/java/com/example/todo_otel/TodoOtelApplication.java
@@ -0,0 +1,13 @@
+package com.example.todo_otel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoOtelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoOtelApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/todo_otel/TodoOtelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todo_otel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoOtelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
